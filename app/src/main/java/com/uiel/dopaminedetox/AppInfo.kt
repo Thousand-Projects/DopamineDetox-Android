1b57735870983a4a1b4c5405450ade813cc94f51
uiel <parkuiery@gmail.com>
2024-07-11 22:45:59 +0900

    ⚡️ :: AppInfo

---
+++ app/src/main/java/com/uiel/dopaminedetox/AppInfo.kt
@@ -0,0 +1,9 @@
+package com.uiel.dopaminedetox
+
+import android.graphics.drawable.Drawable
+
+data class AppInfo(
+    val packageName: String,
+    val appName: String,
+    val appIcon: Drawable,
+)
